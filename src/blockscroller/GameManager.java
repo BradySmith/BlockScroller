516a69e467c53f3a212c2c4a1815e60f60d4861d
Brady Smith <brady_smith@outlook.com>
2014-02-21 14:20:54 -0600

    Slightly working, Movable ball, detects keypresses

--- src/blockscroller/GameManager.java
+++ src/blockscroller/GameManager.java
@@ -5,9 +5,11 @@
  */
 package blockscroller;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.rmi.registry.Registry;
 import javax.swing.JPanel;
 
@@ -15,7 +17,7 @@ import javax.swing.JPanel;
  *
  * @author bradysmith
  */
-public class GameManager extends JPanel implements Runnable {
+public class GameManager extends JPanel implements Runnable, KeyListener {
 
     Game game;
     Player me;
@@ -35,15 +37,19 @@ public class GameManager extends JPanel implements Runnable {
         switch (keyCode) {
             case KeyEvent.VK_UP:
                 me.moveUp();
+                System.out.println("up");
                 break;
             case KeyEvent.VK_DOWN:
                 me.moveDown();
+                System.out.println("down");
                 break;
             case KeyEvent.VK_LEFT:
                 me.moveLeft();
+                System.out.println("left");
                 break;
             case KeyEvent.VK_RIGHT:
                 me.moveRight();
+                System.out.println("right");
                 break;
         }
     }
@@ -61,7 +67,7 @@ public class GameManager extends JPanel implements Runnable {
 
     @Override
     public void run() {
-        this.setBackground(me.getMyColor());
+        this.setBackground(Color.BLACK);
         while (true) {
             if (me == null) {
                 System.out.println("error me is null");
@@ -71,6 +77,14 @@ public class GameManager extends JPanel implements Runnable {
                 repaint();
             }
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
     }
 
 }
